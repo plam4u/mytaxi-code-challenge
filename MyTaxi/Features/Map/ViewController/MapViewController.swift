dd67b2b35aa8858396c99930ef38a6448953755f
Plamen Andreev <plam4u@gmail.com>
2017-09-14 00:44:11 +0200

    Minor map view refactoring

--- MyTaxi/Features/Map/ViewController/MapViewController.swift
+++ MyTaxi/Features/Map/ViewController/MapViewController.swift
@@ -16,14 +16,7 @@ class MapViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
-		mapView.delegate = self
-
-		// Hamburg
-		let location = CLLocationCoordinate2D(latitude: 53.5439092, longitude: 9.9834774)
-		let span = MKCoordinateSpanMake(0.05, 0.05)
-		let region = MKCoordinateRegion(center: location, span: span)
-		mapView.setRegion(region, animated: true)
-
+		setupMapView()
 		refreshPins()
 	}
 
@@ -36,29 +29,46 @@ class MapViewController: UIViewController {
 			// handle error or most likely go to sleep :)
 		}
 	}
+}
 
-	func removeAllPins() {
+// MARK: - MKMapViewDelegate
+
+extension MapViewController: MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+		refreshPins()
+	}
+}
+
+// MARK: - Private methods
+
+extension MapViewController {
+
+	fileprivate func setupMapView() {
+		// Hamburg
+		let location = CLLocationCoordinate2D(latitude: 53.5439092, longitude: 9.9834774)
+		let span = MKCoordinateSpanMake(0.05, 0.05)
+		let region = MKCoordinateRegion(center: location, span: span)
+		mapView.setRegion(region, animated: true)
+
+		mapView.delegate = self
+	}
+
+	fileprivate func removeAllPins() {
 		mapView.removeAnnotations(mapView.annotations)
 	}
 
-	func addPins(for vehicles:[MYTVehicleModel]) {
+	fileprivate func addPins(for vehicles:[MYTVehicleModel]) {
 		vehicles.forEach { [weak self] vehicle in
 			self?.mapView.addAnnotation(createAnnotation(for: vehicle))
 		}
 	}
 
-	func createAnnotation(for vehicle: MYTVehicleModel) -> MKPointAnnotation {
+	fileprivate func createAnnotation(for vehicle: MYTVehicleModel) -> MKPointAnnotation {
 		let location = CLLocationCoordinate2DMake(CLLocationDegrees(vehicle.latitude), CLLocationDegrees(vehicle.longitude))
 		let annotation = MKPointAnnotation()
 		annotation.coordinate = location
 		annotation.title = vehicle.type.capitalized
 		annotation.subtitle = vehicle.state.capitalized
 		return annotation
-	}
-}
-
-extension MapViewController: MKMapViewDelegate {
-	func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-		// query server for new vehicles data
 	}
 }
