23f2a80a6d529c00d316d74d92933bb9d8894d22
Plamen Andreev <plam4u@gmail.com>
2017-09-14 00:40:12 +0200

    Load vehicle data into the map view

--- MyTaxi/Features/Map/ViewController/MapViewController.swift
+++ MyTaxi/Features/Map/ViewController/MapViewController.swift
@@ -16,17 +16,49 @@ class MapViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
+		mapView.delegate = self
+
 		// Hamburg
 		let location = CLLocationCoordinate2D(latitude: 53.5439092, longitude: 9.9834774)
 		let span = MKCoordinateSpanMake(0.05, 0.05)
 		let region = MKCoordinateRegion(center: location, span: span)
 		mapView.setRegion(region, animated: true)
 
+		refreshPins()
+	}
+
+	func refreshPins() {
+		removeAllPins()
+
+		MYTVehiclesAPIClient.shared().getVehiclesIn(mapView.visibleMapRect, success: { [weak self] vehicles in
+			self?.addPins(for: vehicles)
+		}) { (error) in
+			// handle error or most likely go to sleep :)
+		}
+	}
+
+	func removeAllPins() {
+		mapView.removeAnnotations(mapView.annotations)
+	}
+
+	func addPins(for vehicles:[MYTVehicleModel]) {
+		vehicles.forEach { [weak self] vehicle in
+			self?.mapView.addAnnotation(createAnnotation(for: vehicle))
+		}
+	}
+
+	func createAnnotation(for vehicle: MYTVehicleModel) -> MKPointAnnotation {
+		let location = CLLocationCoordinate2DMake(CLLocationDegrees(vehicle.latitude), CLLocationDegrees(vehicle.longitude))
 		let annotation = MKPointAnnotation()
 		annotation.coordinate = location
-		annotation.title = "Taxi"
-		annotation.subtitle = "Active"
-		mapView.addAnnotation(annotation)
+		annotation.title = vehicle.type.capitalized
+		annotation.subtitle = vehicle.state.capitalized
+		return annotation
+	}
+}
 
+extension MapViewController: MKMapViewDelegate {
+	func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+		// query server for new vehicles data
 	}
 }
