f86ac25aa4470411d2978fe9a14f9d3d85b49647
Plamen Andreev <plam4u@gmail.com>
2017-09-13 20:44:47 +0200

    Initial Commit

---
+++ MyTaxi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTaxi
+//
+//  Created by Plamen Andreev on 9/13/17.
+//  Copyright © 2017 Plamen Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTaxi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTaxi
+//
+//  Created by Plamen Andreev on 9/13/17.
+//  Copyright © 2017 Plamen Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
